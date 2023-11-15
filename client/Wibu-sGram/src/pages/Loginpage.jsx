aaa4689c524fb6a48eb7d3a9c037b05a4f3ef00e
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2023-11-15 16:48:46 +0700

    feat: progress-client-login logic

--- client/Wibu-sGram/src/pages/Loginpage.jsx
+++ client/Wibu-sGram/src/pages/Loginpage.jsx
@@ -1,8 +1,37 @@
 import "../App.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import GoogleLoginButton from "../components/LoginGoogle";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const navigate = useNavigate();
+
+	const isEmail = (event) => {
+		setEmail(event.target.value);
+	};
+
+	const isPassword = (event) => {
+		setPassword(event.target.value);
+	};
+
+	const submitLogin = async (event) => {
+		event.preventDefault();
+		try {
+			const response = await axios.post("http://localhost:3000/login", {
+				email,
+				password,
+			});
+			console.log(response, "<<<< response");
+
+			// localStorage.setItem("token", response.data)
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
 	return (
 		<>
 			<section className="login-page">
