38e61e6b9efe76ec10fcebd6658072b6344ecf3f
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2023-11-15 19:41:03 +0700

    feat: progress-client

--- server/routers/main.js
+++ server/routers/main.js
@@ -1,11 +1,21 @@
 const router = require("express").Router();
 const Controller = require("../controllers/controller");
 const authentication = require("../middleware/authentication");
+const TelegramBot = require("node-telegram-bot-api");
+const bot = new TelegramBot("6468894219:AAFh0OPLbPdYl25LwNRyZDMe7mqF_y5St4E", {
+	polling: true,
+});
 
 router.post("/register", Controller.registerPage);
 router.post("/login", Controller.loginPage);
 router.post("/googleLogin", Controller.googleLogin);
-router;
+
+bot.on("message", (msg) => {
+	const chatId = msg.chat.id;
+	const messageText = msg.text;
+
+	if (messageText === "/start") bot.sendMessage(chatId, "WELCOME MY FRIEND!!");
+});
 
 router.use(authentication);
 
