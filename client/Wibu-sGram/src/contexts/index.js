43548d84c29575f6ac26b1046fba577433a8686a
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2023-11-15 10:37:44 +0700

    feat: progress-client

---
+++ client/Wibu-sGram/src/contexts/index.js
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+
+export const LanguageContext = createContext(); //Default value bisa dipasang nanti atau bisa dipasang sekarang
--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const port = process.env.PORT || 3000;
 
@@ -9,6 +10,7 @@ const router = require("./routers/main");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
+app.use(cors);
 
 app.listen(port, () => {
 	console.log(`Mendengarkan Aplikasi pada Port ${port}`);
